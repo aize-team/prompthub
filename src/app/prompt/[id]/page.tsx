16d693e0f338a6d4793989a90e045bba1919d555
Masoud Tavakkoli <mtg1376@gmail.com>
2025-04-26 09:00:31 +0330

    Refactor props type from interface to any in prompt detail page

--- src/app/prompt/[id]/page.tsx
+++ src/app/prompt/[id]/page.tsx
@@ -3,12 +3,6 @@ import { getPromptById, allPrompts } from '@/lib/prompt-data';
 import { notFound } from 'next/navigation';
 import PromptDetailContent from '@/components/PromptDetailContent';
 
-interface PromptDetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
 // Generate static params for all possible prompt IDs
 export function generateStaticParams() {
   return allPrompts.map((prompt) => ({
@@ -17,10 +11,9 @@ export function generateStaticParams() {
 }
 
 // Function to generate metadata dynamically
-export async function generateMetadata({
-  params,
-}: PromptDetailPageProps): Promise<Metadata> {
-  const { id } = await params;
+export async function generateMetadata(props: any): Promise<Metadata> {
+  const params = await props.params;
+  const id = params.id;
   const prompt = getPromptById(id);
 
   if (!prompt) {
@@ -36,8 +29,9 @@ export async function generateMetadata({
 }
 
 // Main export function
-export default async function PromptDetailPage({ params }: PromptDetailPageProps) {
-  const { id } = await params;
+export default async function PromptDetailPage(props: any) {
+  const params = await props.params;
+  const id = params.id;
   const prompt = getPromptById(id);
 
   // If prompt not found, use Next.js notFound function to render 404 page
